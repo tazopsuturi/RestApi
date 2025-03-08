bbc6b659f6b68fb6af332a6573ea7aa8e57b1660
TazoPsuturi <tazopsuturi33@gmail.com>
2025-03-08 23:23:14 +0400

    Started working on new practice. RestAPI

---
+++ src/test/java/GetPosts.java
@@ -0,0 +1,28 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+
+import java.util.List;
+import java.util.Map;
+
+public class GetPosts {
+	
+	public Response response;
+	
+	@Test
+	public void testGetPosts() {
+		response = RestAssured.get("https://jsonplaceholder.typicode.com/posts");
+		
+		JsonPath jsonPath = response.jsonPath();
+		List<Map<String, Object>> firstProd = jsonPath.getList("$");
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String formattedJson = gson.toJson(firstProd);
+		
+		System.out.println(formattedJson);
+		
+	}
+}
